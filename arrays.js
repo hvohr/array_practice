bd5e7e107680925c15851c30d63aa83930c2dbfa
Hollis Vohr <hollis.vohr@gmail.com>
2023-02-07 16:45:49 -0700

    Add index positions explanation

--- arrays.js
+++ arrays.js
@@ -14,3 +14,13 @@ console.log(years)
 // using the shift array method, this removes teh first element in the personsOnline array
 personsOnline.shift();
 console.log(personsOnline)
+
+//Index Positions Explanations + Demostrations
+// In javascript or in many coding languages, each element in an array has a designated index position. In most coding languages
+// the index positions begin with the first element being 0. To find out what position an element is in, you can use the example down below
+// using the breads variable above!
+
+console.log(breads[0])
+
+// This would print out the element with the designated index value of 0 when run in terminal. In this example, the result should be 
+// sourdough, since this is in the first position which has an Index number of 0.
