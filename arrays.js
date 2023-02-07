efeed21a94db0fb74edffb28acffcf4138ca9da4
Hollis Vohr <hollis.vohr@gmail.com>
2023-02-07 16:38:33 -0700

    Put different array methods

--- arrays.js
+++ arrays.js
@@ -1,5 +1,16 @@
 // Arrays Assignment
 var breads = ["Sourdough", "White", "Wheat", "Cinnamon Raisin"];
 var years = [1994, 2008, 2018, 2021]
-var personOnline = [true, false, false, true]
+var personsOnline = [true, false, false, true]
 
+// using the pop array method, this will remove the last element on the breads array
+breads.pop();
+console.log(breads)
+
+//using the push array method, this will add an element to the end of the years array
+years.push (2023)
+console.log(years)
+
+// using the shift array method, this removes teh first element in the personsOnline array
+personsOnline.shift();
+console.log(personsOnline)
